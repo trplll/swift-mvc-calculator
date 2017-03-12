02e3717e19e81715e8569aa5c6012f86121f9ed4
Student <student@students-mac-29.fvtc.edu>
2017-03-12 01:53:13 -0600

    add comments to calculator class

--- calculator/calculator.swift
+++ calculator/calculator.swift
@@ -9,41 +9,41 @@
 import Foundation
 
 class Calculator{
-
+    //declare and initialize Calculator properties
     var Number1: Float = 0.0
     var Number2: Float = 0.0
-
-    
-    
-    
+    // I made Operation variable an enum "optype" instead of a string type where each optype represents one of the calculators available operations
+    var Operation = optype.none
+    //aforementioned enum optype
     enum optype{
     case none
     case add
     case subtract
     case multiply
     case divide
-    
     }
-    var Operation = optype.none
     
+    //calculator clear function reassigns number properties to zero values and sets Operation to optype.none
     
-    
-    func clear(){
+    func clear() ->Void{
         Number1=0.0
         Number2=0.0
         Operation = optype.none
     }
+    
     func Calculate()->Float{
       
+        //calculate function behaves differenly based on the current Operation.optype setting.
         switch Operation{
-        
         case .add:
             return  Number1 + Number2
         case .subtract:
             return  Number1 - Number2
         case .divide:
+            //extra logic to prevent divide by zero crash
             if (Number1 > 0 && Number2 > 0){
-                return  Number1 / Number2}else{return 0}
+                return  Number1 / Number2
+            }else{return 0}
         case .multiply:
             return Number1 * Number2
         case .none:
